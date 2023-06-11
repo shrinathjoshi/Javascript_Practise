480e6e0dfb3735e1b4e5d6cc9a9a3128cedccbff
Shrinath Joshi <shrinathjoshi1997@gmail.com>
2023-06-11 12:33:41 +0530

    Update Pattern.js

--- Pattern.js
+++ Pattern.js
@@ -1,1 +1,25 @@
+/** 
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+
+function generatePattern(n){
+  let pattern='';
+  for(let i=0;i<n;i++){
+    for(let j=0;j<n;j++){
+      pattern+='*'
+    }
+    pattern+='\n';
+  }
+  return pattern;
+}
+
+var pattern  = generatePattern(5);
+console.log(pattern);
+
 
