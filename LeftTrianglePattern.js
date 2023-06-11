4d655e507982319712fb752f782ce3b889c30364
Shrinath Joshi <shrinathjoshi1997@gmail.com>
2023-06-11 12:47:14 +0530

    Create LeftTrianglePattern.js

---
+++ LeftTrianglePattern.js
@@ -0,0 +1,26 @@
+/** 
+
+Left Triangle Pattern in Javascript
+
+*
+**
+***
+****
+*****
+
+*/
+
+
+function generatePattern(n){
+  let pattern='';
+  for(let i=1; i<=n; i++){
+     for(let j=1; j<=i; j++){
+      pattern+='*';
+    }   
+    pattern+='\n';
+  }
+  return pattern;
+}
+
+var pattern  = generatePattern(5);
+console.log(pattern);
